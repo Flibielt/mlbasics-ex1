c597420f20599393bf0aac4a47d1db6b4ce42a64
Csaba Hermann <hcsabi98@gmail.com>
2020-11-29 16:42:19 +0100

    Initial commit

---
+++ REAME.md
@@ -0,0 +1,3 @@
+# Linear Regression & Linear regression with multiple variables
+
+Programming assignment from [Coursera Machine Learning](https://www.coursera.org/learn/machine-learning) rewritten in Python.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+   name='ML - Linear Regression & Linear regression with multiple variables - EX1',
+   version='1.0',
+   description='Linear Regression & Linear regression with multiple variables',
+   author='Csaba Hermann',
+   author_email='hcsabi98@gmail.com',
+   packages=['ex1'],
+   install_requires=[
+       'numpy',
+       'scipy',
+       'matplotlib',
+       'opencv-python'
+   ],
+   scripts=[]
+)
